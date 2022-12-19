953f6440d96f3aed0577848f4790f5583e814a10
GHOST <shahriyar.a13@gmail.com>
2022-12-19 16:27:34 +0100

    project item add seperated

--- lib/presenter/feature/project/add/project_item_add_screen.dart
+++ lib/presenter/feature/project/add/project_item_add_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:kexcel/domain/entity/client_entity.dart';
 import 'package:kexcel/domain/entity/item_entity.dart';
+import 'package:kexcel/domain/entity/project_item_entity.dart';
+import 'package:kexcel/domain/entity/supplier_entity.dart';
 import 'package:kexcel/presenter/base_screen.dart';
+import 'package:kexcel/presenter/common/localization.dart';
 import 'package:kexcel/presenter/data_load_bloc_builder.dart';
 import 'package:kexcel/presenter/feature/project/add/project_item_add_bloc.dart';
 import 'package:kexcel/presenter/feature/project/add/project_item_add_bloc_event.dart';
+import 'package:kexcel/presenter/feature/project/pdf_screen.dart';
 import 'package:kexcel/presenter/widget/no_item_widget.dart';
 
 class ProjectItemAddScreen extends BaseScreen<ProjectItemAddBloc> {
-  const ProjectItemAddScreen({super.key});
+  final ProjectItemEntity? entity;
+  const ProjectItemAddScreen({this.entity, super.key});
 
   @override
   AppBar? get appBar => AppBar();
@@ -18,11 +24,122 @@ class ProjectItemAddScreen extends BaseScreen<ProjectItemAddBloc> {
   @override
   Widget screenBody(BuildContext context) {
     callEvent(ProjectItemAddEventInit());
+
+    final TextEditingController nameController = TextEditingController(text: entity?.name);
+
+    ClientEntity? client = entity?.client;
+    SupplierEntity? winner = entity?.winner;
+
     return DataLoadBlocBuilder<ProjectItemAddBloc, List<ItemEntity>?>(
         noDataView: const NoItemWidget(),
         bloc: getBloc,
         builder: (BuildContext context, List<ItemEntity>? entities) {
-          return Container();
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const SizedBox(height: 25),
+                        TextField(
+                          controller: nameController,
+                          decoration: InputDecoration(
+                            border: const OutlineInputBorder(),
+                            labelText: 'name'.translate,
+                          ),
+                        ),
+                        const SizedBox(height: 25),
+                        InputDecorator(
+                          decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.symmetric(
+                                vertical: 0, horizontal: 4),
+                            border: const OutlineInputBorder(gapPadding: 1),
+                            labelText: 'client'.translate,
+                          ),
+                          child: Autocomplete(
+                            onSelected: (ClientEntity entity) =>
+                                client = entity,
+                            displayStringForOption: (ClientEntity entity) =>
+                                entity.name,
+                            initialValue:
+                                TextEditingValue(text: client?.name ?? ''),
+                            optionsBuilder:
+                                (TextEditingValue textEditingValue) {
+                              return getBloc.clients
+                                  .where(
+                                    (ClientEntity entity) =>
+                                        (entity.name.toLowerCase().contains(
+                                                  textEditingValue.text
+                                                      .toLowerCase(),
+                                                ) ||
+                                            entity.code.toLowerCase().contains(
+                                                  textEditingValue.text
+                                                      .toLowerCase(),
+                                                )),
+                                  )
+                                  .toList();
+                            },
+                          ),
+                        ),
+                        const SizedBox(height: 25),
+                        InputDecorator(
+                          decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.symmetric(
+                                vertical: 0, horizontal: 4),
+                            border: const OutlineInputBorder(gapPadding: 1),
+                            labelText: 'winner'.translate,
+                          ),
+                          child: Autocomplete(
+                            onSelected: (SupplierEntity entity) =>
+                                winner = entity,
+                            displayStringForOption: (SupplierEntity entity) =>
+                                entity.name,
+                            initialValue:
+                                TextEditingValue(text: winner?.name ?? ''),
+                            optionsBuilder:
+                                (TextEditingValue textEditingValue) {
+                              return getBloc.suppliers
+                                  .where(
+                                    (SupplierEntity entity) => (entity.name
+                                        .toLowerCase()
+                                        .contains(
+                                          textEditingValue.text.toLowerCase(),
+                                        )),
+                                  )
+                                  .toList();
+                            },
+                          ),
+                        ),
+                        const SizedBox(height: 25),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    final newEntity = ProjectItemEntity(
+                      projectId: 0,
+                      id: 0,
+                      client: client,
+                      winner: winner,
+                      name: nameController.text,
+                      karaProjectNumber: 0,
+                    );
+
+                    callEvent(ProjectItemEventAddingDone(newEntity));
+
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const PDFScreen(),
+                    ));
+                  },
+                  child: Text('save'.translate))
+            ],
+          );
         });
   }
 }
--- lib/presenter/feature/project/item/project_item_screen.dart
+++ lib/presenter/feature/project/item/project_item_screen.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:kexcel/domain/entity/client_entity.dart';
 import 'package:kexcel/domain/entity/project_item_entity.dart';
-import 'package:kexcel/domain/entity/supplier_entity.dart';
 import 'package:kexcel/presenter/base_bloc_event.dart';
 import 'package:kexcel/presenter/common/localization.dart';
 import 'package:kexcel/presenter/feature/information/base_information_screen.dart';
+import 'package:kexcel/presenter/feature/project/add/project_item_add_screen.dart';
 import 'package:kexcel/presenter/utils/excel_utils.dart';
 import 'project_item_bloc.dart';
 import 'project_item_bloc_event.dart';
@@ -55,107 +54,9 @@ class ProjectItemScreen extends BaseInformationScreen<ProjectItemBloc, ProjectIt
 
   @override
   void editItemDetails(BuildContext context, {ProjectItemEntity? entity}) {
-    final TextEditingController nameController =
-    TextEditingController(text: entity?.name ?? '');
-
-    ClientEntity? client = entity?.client;
-    SupplierEntity? winner = entity?.winner;
-
-    showModalBottomSheet(
-      isScrollControlled: true,
-      context: context,
-      builder: (context) =>
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const SizedBox(height: 25),
-                  TextField(
-                    controller: nameController,
-                    decoration: InputDecoration(
-                      border: const OutlineInputBorder(),
-                      labelText: 'name'.translate,
-                    ),
-                  ),
-                  const SizedBox(height: 25),
-                  InputDecorator(
-                    decoration: InputDecoration(
-                      contentPadding:
-                      const EdgeInsets.symmetric(vertical: 0, horizontal: 4),
-                      border: const OutlineInputBorder(gapPadding: 1),
-                      labelText: 'client'.translate,
-                    ),
-                    child: Autocomplete(
-                      onSelected: (ClientEntity entity) => client = entity,
-                      displayStringForOption: (ClientEntity entity) =>
-                      entity.name,
-                      initialValue: TextEditingValue(text: client?.name ?? ''),
-                      optionsBuilder: (TextEditingValue textEditingValue) {
-                        return getBloc.clients
-                            .where(
-                              (ClientEntity entity) =>
-                          (entity.name.toLowerCase().contains(
-                            textEditingValue.text.toLowerCase(),
-                          ) ||
-                              entity.code.toLowerCase().contains(
-                                textEditingValue.text.toLowerCase(),
-                              )),
-                        )
-                            .toList();
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 25),
-                  InputDecorator(
-                    decoration: InputDecoration(
-                      contentPadding:
-                      const EdgeInsets.symmetric(vertical: 0, horizontal: 4),
-                      border: const OutlineInputBorder(gapPadding: 1),
-                      labelText: 'winner'.translate,
-                    ),
-                    child: Autocomplete(
-                      onSelected: (SupplierEntity entity) => winner = entity,
-                      displayStringForOption: (SupplierEntity entity) =>
-                      entity.name,
-                      initialValue: TextEditingValue(text: winner?.name ?? ''),
-                      optionsBuilder: (TextEditingValue textEditingValue) {
-                        return getBloc.suppliers
-                            .where(
-                              (SupplierEntity entity) =>
-                          (entity.name.toLowerCase().contains(
-                            textEditingValue.text.toLowerCase(),
-                          )),
-                        )
-                            .toList();
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 25),
-                  ElevatedButton(
-                      onPressed: () {
-                        final newEntity = ProjectItemEntity(
-                          projectId: 0,
-                          id: entity?.id ?? -1,
-                          client: client,
-                          winner: winner,
-                          name: nameController.text,
-                          karaProjectNumber: 0,
-                        );
-                        if (entity == null) {
-                          callEvent(ProjectItemEventAddingDone(newEntity));
-                        } else {
-                          callEvent(ProjectItemEventEditingDone(newEntity));
-                        }
-                        Navigator.pop(context);
-                      },
-                      child: Text('save'.translate))
-                ],
-              ),
-            ),
-          ),
-    );
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => ProjectItemAddScreen(entity: entity),
+    ));
   }
 
   @override
